8866681bde820f8c8e030887f6e3ec2d2c4a2dce
thaisdeveloper <thaisocialmedia.job@gmail.com>
2023-08-15 14:45:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const btnGenerate = document.querySelector("#generate-pdf");
+
+btnGenerate.addEventListener("click",()=>{
+
+    //Conteúdo do PDF
+    const content = document.querySelector("#content");
+
+    // Configuração do arquivo final do PDF
+    const options = {
+        margin: [10, 10, 10, 10],
+        filename: "arquivo.pdf",
+        html2canvas: {scale: 2},
+        jsPDF: {unit: "mm",format: "a4", orientation: "portrait" }
+    }
+
+    //Gerar PDF
+html2pdf().set(options).from(content).save();
+})
